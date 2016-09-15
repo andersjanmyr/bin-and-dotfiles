509048e811d51064933b00bc148f5429cfac927d
Anders Janmyr <anders@janmyr.com>
2016-09-15 09:39:48 -0700

    Moved google-cloud completions to completion file

---
+++ dotfiles/bash/completions/google-cloud-sdk.sh
@@ -0,0 +1,2 @@
+source '/usr/local/Homebrew/Caskroom/google-cloud-sdk/latest/google-cloud-sdk/path.bash.inc'
+source '/usr/local/Homebrew/Caskroom/google-cloud-sdk/latest/google-cloud-sdk/completion.bash.inc'
