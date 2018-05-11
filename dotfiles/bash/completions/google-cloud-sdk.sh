ab52114e392d68dd60c2c3f2b225fdc154f8aa6f
Anders Janmyr <anders@janmyr.com>
2018-05-11 08:20:54 -0700

    Removed google sdk completion

--- dotfiles/bash/completions/google-cloud-sdk.sh
+++
@@ -1,2 +0,0 @@
-source '/usr/local/Homebrew/Caskroom/google-cloud-sdk/latest/google-cloud-sdk/path.bash.inc'
-source '/usr/local/Homebrew/Caskroom/google-cloud-sdk/latest/google-cloud-sdk/completion.bash.inc'
