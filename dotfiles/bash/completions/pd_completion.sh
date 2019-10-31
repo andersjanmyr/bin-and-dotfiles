2af9cfff8510901a3a380299847de53873877d07
Anders Janmyr <anders@janmyr.com>
2019-10-31 17:08:10 +0100

    Fixed pd

--- dotfiles/bash/completions/pd_completion.sh
+++ dotfiles/bash/completions/pd_completion.sh
@@ -1,9 +1,8 @@
-
 _pd() {
     local cmd=${COMP_WORDS[0]}
     local cur=${COMP_WORDS[COMP_CWORD]}
 
-    project_dir=`ls -d ~/Projects/*/ ~/External/*/*/ | xargs -n1 basename`
+    project_dir=`find ~/Projects -print -type d -maxdepth 2 | tail +2 | xargs -n1 basename`
     words="$project_dir"
 
     COMPREPLY=($(compgen -W "$words" -- $cur))
