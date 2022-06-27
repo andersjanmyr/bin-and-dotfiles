b8fddf04063c37fcb27a47df7082c763d26b36be
Anders Janmyr <anders@janmyr.com>
2022-06-27 10:13:46 +0200

    Added help text

--- sql_csv.sh
+++ sql_csv.sh
@@ -1,7 +1,13 @@
-#!/bin/bash -x
+#!/bin/bash
 
 set -o errexit
 
+if [[ $# -lt 1 ]]; then
+  echo "Usage: $0 <table> [query]"
+  echo "option: output=<one of>"
+  sqlite3 :memory: -cmd '.help mode' .exit
+  exit 1
+fi
 table=${1?Table name is required}
 table=${table%.*}
 query=${2:-"select * from $table"}
