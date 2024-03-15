b65d652f4f3ed8c054a5f26046654640b4477aac
Anders Janmyr <anders@janmyr.com>
2024-03-15 12:34:13 +0100

    Updated couch_active_tasks

--- couch_active_tasks.sh
+++ couch_active_tasks.sh
@@ -3,11 +3,11 @@
 filter=${1:-""}
 
 couchp () {
-    curl --silen -H 'Content-Type:application/json' ${COUCH_PROD}$1 $2 $3 $4 $5 $6 $7
+    curl --silent -H 'Content-Type:application/json' ${COUCH_PROD}$1 $2 $3 $4 $5 $6 $7
 }
 
 couchp /_active_tasks \
-    | jq -r -c '.[] | "\(.node) \(.process_status) \(.changes_done)/\(.total_changes) \(.changes_done*100/.total_changes|round) \(.database) \(.design_document)"' \
+    | jq -r -c '.[] | "\(.node) \(.process_status) \(.changes_done)/\(.total_changes) \(.changes_done*100/.total_changes|round) \(.database) \(.type) \(.design_document)"' \
     | grep "$filter" \
     | sed 's/couchdb@//' | sed 's/\.smrt\.internal//' | sed 's#shards/00000000-ffffffff/##' \
     | column -t
