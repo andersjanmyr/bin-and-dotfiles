b1e3281122912e3cdc2e4b5996fade7ed78e8b1f
Anders Janmyr <anders@janmyr.com>
2024-03-18 11:21:01 +0100

    Include replication

--- couch_active_tasks.sh
+++ couch_active_tasks.sh
@@ -6,8 +6,15 @@ couchp () {
     curl --silent -H 'Content-Type:application/json' ${COUCH_PROD}$1 $2 $3 $4 $5 $6 $7
 }
 
-couchp /_active_tasks \
-    | jq -c '.[]' \
+tasks=$(couchp /_active_tasks | jq -c '.[]')
+
+echo "$tasks" \
+    | grep "replication" \
+    | grep "$filter" \
+    | jq -r '"\(.node) \(.process_status) \(.docs_read)/\(.docs_written)/\(.doc_write_failures) \(.changes_pending) \(.doc_id) \(.type)"' \
+    | sed 's/couchdb@//' | sed 's/\.smrt\.internal//' | sed 's#shards/00000000-ffffffff/##' \
+    | column -t
+echo "$tasks" \
     | grep -v "replication" \
     | grep "$filter" \
     | jq -r '"\(.node) \(.process_status) \(.changes_done)/\(.total_changes) \(.changes_done*100/.total_changes|round) \(.database) \(.type) \(.design_document)"' \
