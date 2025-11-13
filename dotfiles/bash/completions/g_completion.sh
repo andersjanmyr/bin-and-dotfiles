71b8a0a11be01c9767c20c81ada7b5d60b124b2b
Anders Janmyr <anders@janmyr.com>
2025-11-13 09:36:01 +0100

    Improved git-squash with completion

--- dotfiles/bash/completions/g_completion.sh
+++ dotfiles/bash/completions/g_completion.sh
@@ -1,2 +1,10 @@
+source /opt/homebrew/etc/bash_completion.d/git-completion.bash
+
+_git_squash() {
+    __gitcomp_nl "$(__git_refs)"
+}
+
+# Register the completion
+__git_complete squash _git_squash
 
 __git_complete g __git_main
