eb607ef97e40e00e58060deb96af4523694308e9
Anders Janmyr <anders@janmyr.com>
2025-11-24 15:21:46 +0100

    fixed util scripts

--- biz.sh
+++ biz.sh
@@ -2,10 +2,16 @@
 
 pattern=${1?'pattern is required'}
 number='^[0-9]+$'
+query=${2}
 if [[ $pattern =~ $number ]]; then
     pattern="\"bid\":$pattern,";
+    if [[ -z $query ]]; then
+        query='.subdomain'
+    fi
 fi
-query=${2:-'.subdomain'}
+if [[ -z $query ]]; then
+    query='.bid'
+fi
 env=${env:-prod}
 
 prod_envs=$(ls ~/tmp/phpweb-prod{3,5,6}.json ~/tmp/apac.json ~/tmp/emea.json)
--- db.sh
+++ db.sh
@@ -5,7 +5,8 @@ set -o errexit
 biz=${1?'biz is required'}
 sql_file=${2:-'script.sql'}
 user=${user:-'smrtreadonly'}
+DB_HOST=${DB_HOST:-mysql-ro-prod5}
 used_implicitly=${MYSQL_PWD?'is required'}
 
 echo $biz
-mysql --verbose -A -u $user -h 127.0.0.1 -P 3306 -D "${biz}_prod" < "$sql_file"
+mysql --verbose -A -u $user -h $DB_HOST -P 3306 -D "${biz}_prod" < "$sql_file"
